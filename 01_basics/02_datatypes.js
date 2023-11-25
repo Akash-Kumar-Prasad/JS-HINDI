c9062ea7bde85ce9c78b51fe92c5027b584a82df
Akash Kumar Prasad <99805268+Akash-Kumar-Prasad@users.noreply.github.com>
2023-11-25 22:06:51 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";
+//treat all js code as newer version
+ // alert(3+3) //we sre using nodejs not browser
+
+ console.log(3+3)
+//code readibility should be high
+ console.log("Akash ")
+ let name= "Akumar"
+ let age=25
+ let isLoggedIn= false//number=>2 to power of 53
+ //bigint
+ //string =>""
+ //boolean =>true/false
+ //null, a standalone value, an object, representation of empty value, its a type, it means intentionally it has be left
+ // undefined=> its type is also "undefined", variable has been declared but value is not assigned
+ //symbol = >unique
+
+//objects
+
+console.log(typeof"Akumar")
+console.log(typeof(null));
+console.log(typeof(undefined));
