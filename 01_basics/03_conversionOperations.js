ea96a4cc1c78556c08fb4a550facdd9ff1dc248a
Akash Kumar Prasad <99805268+Akash-Kumar-Prasad@users.noreply.github.com>
2023-11-29 15:20:08 +0000

    stringConversion

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -14,19 +14,19 @@ let valueInNumber = Number(score);
 
 let isLoggedIn1 = 1
 let booleanIsLoggedIn1 = Boolean(isLoggedIn1);
-console.log(booleanIsLoggedIn1);
+// console.log(booleanIsLoggedIn1);
 
 let isLoggedIn0 = 0
 let booleanIsLoggedIn0 = Boolean(isLoggedIn0);
-console.log(booleanIsLoggedIn0);
+// console.log(booleanIsLoggedIn0);
 
 let isLoggedInEmptyString = ""
 let booleanIsLoggedInEmptyString = Boolean(isLoggedInEmptyString);
-console.log(booleanIsLoggedInEmptyString);
+// console.log(booleanIsLoggedInEmptyString);
 
 let isLoggedInString = "akash"
 let booleanIsLoggedInString = Boolean(isLoggedInString);
-console.log(booleanIsLoggedInString);
+// console.log(booleanIsLoggedInString);
 
 //1 => true
 //0 => false
@@ -35,4 +35,39 @@ console.log(booleanIsLoggedInString);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
+// console.log(stringNumber);
+
+//*****Operations******
+
+let value = 32
+let negValue = -value
+console.log(negValue)
+
+console.log(2+2);
+console.log(2-2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**3);
+
+let str1 = "hello"
+let str2 = " akash"
+let str3 = str1+str2;
+console.log(str3);
+
+console.log(1 + 2);//3
+console.log("1" + 2 );//12
+console.log(1 + "2");//12
+console.log(1 + 2 + "3");//33
+console.log("1"+ 2 + 3);//123
+console.log("1"+"2"+3 + 4);//1234
+
+console.log(true)
+console.log(+true);
+console.log(+"");
+let num1, num2,num3
+num1=num2=num3=2+2
+
+let gameCounter= 100
+gameCounter++;
+console.log(gameCounter);
+
