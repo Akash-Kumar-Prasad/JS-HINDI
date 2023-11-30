265f2976f664add21f19bd6ebf5291c3150c3c36
Akash Kumar Prasad <99805268+Akash-Kumar-Prasad@users.noreply.github.com>
2023-11-30 11:01:01 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,40 @@
+// Primitive
+//call by value(changes are made to the copied item)
+// 7 types
+//String, Number, Boolean, null, undefined, Symbol, bigInt
+
+
+const score = 100
+const scoreValue= 100.25
+
+const isLoggedIn= false
+const outsideTemp = null
+let userEmail;
+
+const id= Symbol('123')
+const anotherId= Symbol('123')
+
+console.log(id===anotherId);
+
+const bigNumber= 32165498798765465432321n;
+
+
+//Non-Primitive
+//call by reference(changes are made to the actual value)
+//Arrays , Objects, Functions
+
+const heroes= ["shaktimaan", "nagaraj", "doga"]
+
+let myObj = {
+    name : "akash",
+    age: 27
+}
+
+const myFunction = function (){
+console.log("Hllo Eorld");
+}
+
+
+console.log(typeof heroes);
+console.log(typeof myObj);
+console.log(typeof myFunction);
