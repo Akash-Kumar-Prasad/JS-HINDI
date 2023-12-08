d5b117dee43f0cd1b2c18d3a12ccfb580213caa3
Akash-Kumar-Prasad <iakashprasad@gmail.com>
2023-12-08 21:10:26 +0530

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,16 @@
+// if statement, works as you want it to
+// if(true){
+// //internal code will run
+// }
+// if (false) {
+//   //will not run,
+// }
+// <, > , <=, >=, ==, !=, ===, !==
+ let temperature = 41
+ if(temperature === 41){
+  console.log("less than 50");
+ }else{
+  console.log("temperature is greater than 50");
+ }
+
+ console.log("Execute");
