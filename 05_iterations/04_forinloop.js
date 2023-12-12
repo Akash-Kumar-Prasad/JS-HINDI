b4698ac8d628debc6a2c0cb72917762b7f0480dd
Akash-Kumar-Prasad <iakashprasad@gmail.com>
2023-12-12 14:39:56 +0530

    for in loop

---
+++ 05_iterations/04_forinloop.js
@@ -0,0 +1,35 @@
+const myObject = {
+  js : "javascript", 
+  cpp:"c++",
+  rb:"ruby",
+  swift:"swift by apple"
+}
+
+for (const key in myObject) {
+ console.log(key);
+}
+
+for (const key in myObject) {
+ console.log(`${key} is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb","py","java", "cpp"]
+
+for (const key in programming) {
+  console.log(key);
+}
+
+for(const key in programming){
+  console.log(programming[key]);
+}
+
+const map = new Map();
+map.set('IN', 'India')
+map.set('USA', 'United States of America')
+map.set('JP', 'Japan')
+map.set('FR', 'France')
+
+for (const key in map) {
+  console.log(key);//cannot iterate
+}
+
